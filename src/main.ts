a1200e5fdb1945b71052d3504492cca9eb49c7d5
CodinCat <a55951234@gmail.com>
2020-02-28 18:16:23 +0900

    Rename GameWorld to Game

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import Brawler from './Entities/Brawler'
-import GameWorld from './Systems/GameWorld'
+import Game from './Systems/Game'
 
 const canvasElement = <HTMLCanvasElement>document.getElementById('canvas')
 
 const b1 = new Brawler()
 const b2 = new Brawler({ x: 30, y: 30 }, 'red', true)
 
-const game = new GameWorld(canvasElement, [b1, b2])
+const game = new Game(canvasElement, [b1, b2])
 game.start()
